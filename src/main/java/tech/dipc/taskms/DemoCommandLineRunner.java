57d219da82537becc90cf740e545b7943030f705
MGirisha2001 <67091261+MGirisha2001@users.noreply.github.com>
2020-08-05 16:15:35 +0530

    Taskms Task

---
+++ src/main/java/tech/dipc/taskms/DemoCommandLineRunner.java
@@ -0,0 +1,181 @@
+package tech.dipc.taskms;
+
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import tech.dipc.taskms.task.TaskEmployee;
+import tech.dipc.taskms.task.TaskRepo;
+import tech.dipc.taskms.user.UserEmployee;
+import tech.dipc.taskms.user.UserRepo;
+
+
+@SpringBootApplication
+
+public class DemoCommandLineRunner implements CommandLineRunner {
+	
+	 public static void main(String[] args) {
+	      SpringApplication.run(TaskmsApplication.class, args);
+	   }
+	   
+	   @Autowired
+	   private TaskRepo repos;
+	   
+
+	   @Autowired
+	   private UserRepo repo;
+	   
+	   @Override
+	   public void run(String... arg0) throws Exception {
+	      System.out.println("Data Loaded by CommandLineRunner for Task.");
+	      System.out.println("Data Loaded by CommandLineRunner for User.");
+	      
+	      
+	      TaskEmployee e1=new TaskEmployee();
+			
+			e1.setEid(1);
+			e1.setTaskName("Task Management System");
+			e1.setCreator("Girisha");
+			e1.setAssignedTo("John");
+			e1.setProposedStartTime(10);
+			e1.setActualStartTime(11);
+			e1.setEstimatedTimeTaken(5);
+			e1.setActualTimeTaken(6);
+			
+			
+	TaskEmployee e2=new TaskEmployee();
+			
+	e2.setEid(2);
+	e2.setTaskName("Testing");
+	e2.setCreator("Smith");
+	e2.setAssignedTo("Andrew");
+	e2.setProposedStartTime(3);
+	e2.setActualStartTime(5);
+	e2.setEstimatedTimeTaken(3);
+	e2.setActualTimeTaken(5);
+
+			
+			
+	TaskEmployee e3=new TaskEmployee();
+
+	e3.setEid(3);
+	e3.setTaskName("Crud operation");
+	e3.setCreator("Helen");
+	e3.setAssignedTo("Saw");
+	e3.setProposedStartTime(10);
+	e3.setActualStartTime(11);
+	e3.setEstimatedTimeTaken(5);
+	e3.setActualTimeTaken(6);
+
+			
+	TaskEmployee e4=new TaskEmployee();
+
+	e4.setEid(4);
+	e4.setTaskName("Command Line Runner");
+	e4.setCreator("Nithya");
+	e4.setAssignedTo("Nivya");
+	e4.setProposedStartTime(5);
+	e4.setActualStartTime(6);
+	e4.setEstimatedTimeTaken(5);
+	e4.setActualTimeTaken(3);
+
+			
+	TaskEmployee e5=new TaskEmployee();
+	e5.setEid(5);
+	e5.setTaskName("Sql");
+	e5.setCreator("Jyothi");
+	e5.setAssignedTo("Priya");
+	e5.setProposedStartTime(11);
+	e5.setActualStartTime(12);
+	e5.setEstimatedTimeTaken(3);
+	e5.setActualTimeTaken(4);
+	
+	
+	
+	UserEmployee emp1=new UserEmployee();
+	
+	emp1.setCid(1);
+	emp1.setFirstname("Ronnie");
+	emp1.setMiddlename("D.");
+	emp1.setLastname("Anderson");
+	emp1.setEmail("Ronnie@gmail.com");
+	emp1.setPhonenum(987654321);
+	emp1.setRole("Developer");
+	
+	
+UserEmployee emp2=new UserEmployee();
+	
+	emp2.setCid(2);
+	emp2.setFirstname("John");
+	emp2.setMiddlename("Andrew");
+	emp2.setLastname("Brown");
+	emp2.setEmail("John@gmail.com");
+	emp2.setPhonenum(834509384);
+	emp2.setRole("Tester");
+	
+	
+UserEmployee emp3=new UserEmployee();
+
+	emp3.setCid(3);
+	emp3.setFirstname("Ann");
+	emp3.setMiddlename("Claire");
+	emp3.setLastname("Brook");
+	emp3.setEmail("Ann@gmail.com");
+	emp3.setPhonenum(984312345);
+	emp3.setRole("Developer");
+	
+UserEmployee emp4=new UserEmployee();
+
+	emp4.setCid(4);
+	emp4.setFirstname("Frank");
+	emp4.setMiddlename("Finn");
+	emp4.setLastname("Hill");
+	emp4.setEmail("Frank@gmail.com");
+	emp4.setPhonenum(932267890);
+	emp4.setRole("tester");
+	
+	
+UserEmployee emp5=new UserEmployee();
+	
+	emp5.setCid(5);
+	emp5.setFirstname("Betty");
+	emp5.setMiddlename("Grace");
+	emp5.setLastname("Johnson");
+	emp5.setEmail("Betty@gmail.com");
+	emp5.setPhonenum(973212345);;
+	emp5.setRole("Developer");
+	
+
+			
+			ArrayList<TaskEmployee> display= new ArrayList();
+			display.add(e1);
+			display.add(e2);
+			display.add(e3);
+			display.add(e4);
+			display.add(e5);
+			
+			repos.saveAll(display);
+			
+			
+			ArrayList<UserEmployee> emp= new ArrayList();
+			emp.add(emp1);
+			emp.add(emp2);
+			emp.add(emp3);
+			emp.add(emp4);
+			emp.add(emp5);
+			
+			repo.saveAll(emp);
+			
+	  }
+	   
+	   
+	   
+	   
+	   }
+	
+
--- src/test/java/tech/dipc/taskms/TaskmsApplicationTests.java
+++ src/test/java/tech/dipc/taskms/TaskmsApplicationTests.java
@@ -1,6 +1,7 @@
 
 package tech.dipc.taskms;
 
+
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -13,19 +14,19 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.test.context.junit4.SpringRunner;
 
 
+import tech.dipc.taskms.task.TaskEmployee;
+import tech.dipc.taskms.user.UserEmployee;
 @RunWith(SpringRunner.class)
-
-@SpringBootTest(classes = TaskmsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest (classes = TaskmsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class TaskmsApplicationTests {
-	
-
-	     @Autowired
+	@Autowired
      private TestRestTemplate restTemplate;
 
      @LocalServerPort
@@ -40,8 +41,11 @@ public class TaskmsApplicationTests {
 
      }
 
-    @Test
-     public void testGetAllEmployees() throws Exception{
+                              // Testing for Task
+     
+     
+      @Test
+     public void testGetDetails() throws Exception{
      HttpHeaders headers = new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
         ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/details",
@@ -50,9 +54,10 @@ public class TaskmsApplicationTests {
     }
   	
         		
-    @Test
-     public void testPostAllEmployees() throws Exception{
-         HttpHeaders headers = new HttpHeaders();
+      @Test
+     public void testPostDetails() throws Exception{
+    	
+    	 HttpHeaders headers = new HttpHeaders();
             HttpEntity<String> entity = new HttpEntity<String>(null, headers);
             ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/postdetails",
             HttpMethod.POST, entity, String.class);  
@@ -60,20 +65,73 @@ public class TaskmsApplicationTests {
      }
      
      
-     @Test
-     public void testDeleteAllEmployees() throws Exception{
-         HttpHeaders headers = new HttpHeaders();
-            HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-            ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/details/{eid}",
-            HttpMethod.DELETE, entity, String.class);  
-            assertNotNull(response.getBody());
-        }
+      @Test
+     public void testDeleteDetails() throws Exception{
      
-     @Test
-     public void testPutAllEmployees() throws Exception{
+            int eid = 2;
+            UserEmployee del1 = restTemplate.getForObject(getRootUrl() + "/employee/" + eid, UserEmployee.class);
+            assertNotNull(del1);
+            restTemplate.delete(getRootUrl() + "/employee/" + eid);
+           del1 = restTemplate.getForObject(getRootUrl() + "/employee/" + eid, UserEmployee.class);
+           }
+
+      
+      @Test
+     public void testPutDetails() throws Exception{
          HttpHeaders headers = new HttpHeaders();
             HttpEntity<String> entity = new HttpEntity<String>(null, headers);
             ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/putdetails",
+            HttpMethod.PUT, entity, String.class);  
+            assertNotNull(response.getBody());
+     }
+     
+     
+     
+     
+     
+                                   // Testing for User
+     
+     
+     
+     
+     @Test
+     public void testGetEmployee() throws Exception{
+     HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/employee",
+        HttpMethod.GET, entity, String.class);  
+        assertNotNull(response.getBody());
+    }
+  	
+        		
+   @Test
+     public void testPostEmployee() throws Exception{
+    	
+    	 HttpHeaders headers = new HttpHeaders();
+            HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+            ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/postemployee",
+            HttpMethod.POST, entity, String.class);  
+            assertNotNull(response.getBody());
+     }
+     
+     
+   @Test
+     public void testDeleteEmployee() throws Exception{
+	   
+	   
+	   int cid=1;
+       TaskEmployee del2 = restTemplate.getForObject(getRootUrl() + "/details/" + cid, TaskEmployee.class);
+       assertNotNull(del2);
+       restTemplate.delete(getRootUrl() + "/details/" + cid);
+       del2 = restTemplate.getForObject(getRootUrl() + "/details/" + cid, TaskEmployee.class);
+      }
+     
+   
+     @Test
+     public void testPutEmployee() throws Exception{
+         HttpHeaders headers = new HttpHeaders();
+            HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+            ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/putemployee",
             HttpMethod.PUT, entity, String.class);  
             assertNotNull(response.getBody());
      }
@@ -84,4 +142,5 @@ public class TaskmsApplicationTests {
 
 
 
+    
      
